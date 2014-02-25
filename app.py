cf0a832f18e694be60123027887f76dde8fda55a
Ben Edelstein <Ben@dyn-160-39-13-174.dyn.columbia.edu>
2014-02-25 01:23:47 -0500

    set up flask and directory structure

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify, render_template
+import requests 
+
+app = Flask(__name__)
+app.config["DEBUG"] = True 
+
+@app.route("/")
+def hello():
+    return render_template("home.html")
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return "Whoops." , 404 
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
